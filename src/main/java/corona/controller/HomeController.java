fadbe093da71ef6c08c24413aa3bef859763af34
Mayur.lad <mayur.lad@nendrasys.com>
2020-08-24 11:31:13 +0530

    project Base setup

---
+++ src/main/java/corona/controller/HomeController.java
@@ -0,0 +1,16 @@
+package corona.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String homepage(Model model) {
+        model.addAttribute("message", "welcome to corona fighter's club");
+        return "homepage";
+    }
+}
