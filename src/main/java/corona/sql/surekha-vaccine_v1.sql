276e4a36df171e53e3c05bf4582c628975c4047f
pallavidongre <66672147+pallavidongre@users.noreply.github.com>
2020-09-01 17:10:38 +0530

    updated vaccine.sql file

---
+++ src/main/java/corona/sql/surekha-vaccine_v1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `corona`.`vaccine` (
+  `vaccineId` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `price` DOUBLE NULL,
+  `quantity` INT NULL,
+  PRIMARY KEY (`vaccineId`));
