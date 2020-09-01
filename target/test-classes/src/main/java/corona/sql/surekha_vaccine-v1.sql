185888064d9bd8ef035ff6a83a5602ec2c00ed2d
pallavidongre <66672147+pallavidongre@users.noreply.github.com>
2020-09-01 17:03:17 +0530

    Revert "Revert "Revert "added only vaccine.sql file.."""
    This reverts commit 15791065a576f7e1f1da586a5145ae57ac18871b.

--- target/test-classes/src/main/java/corona/sql/surekha_vaccine-v1.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `corona`.`vaccine` (
-  `vaccineId` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `price` DOUBLE NULL,
-  `quantity` INT NULL,
-  PRIMARY KEY (`vaccineId`));
