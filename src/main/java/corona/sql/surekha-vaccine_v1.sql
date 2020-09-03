00eb8b9fa7cd71f43be05a87d65bb6aa01979ab0
pallavidongre <66672147+pallavidongre@users.noreply.github.com>
2020-09-03 15:08:31 +0530

    updated vaccine.sql file by adding data into vaccine table

--- src/main/java/corona/sql/surekha-vaccine_v1.sql
+++ src/main/java/corona/sql/surekha-vaccine_v1.sql
@@ -1,6 +1,15 @@
-CREATE TABLE `corona`.`vaccine` (
-  `vaccineId` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `price` DOUBLE NULL,
-  `quantity` INT NULL,
-  PRIMARY KEY (`vaccineId`));
+
+DROP TABLE IF EXISTS `vaccine`;
+
+CREATE TABLE `vaccine` (
+  `vaccineId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`vaccineId`)
+) ;
+LOCK TABLES `vaccine` WRITE;
+
+INSERT INTO `vaccine` VALUES (1001,'Oxford-AstraZeneca\'s COVID-19 vaccine, Developed by UK',1000,1),(1002,'Sputnik V, Developed by Russia',800,1),(1003,'COVAXIN, Developed by Bharat Biotech International Limited India',500,1),(1004,'COVAX-19, Developed by Flinders University Australia',600,1);
+
+UNLOCK TABLES;
