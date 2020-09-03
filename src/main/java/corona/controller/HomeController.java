ef45ce68b8e8725dd0e88a2e1af662d23802b39b
sauravkr827 <sauravkr82711@gmail.com>
2020-09-03 14:38:23 +0530

    commiting

--- src/main/java/corona/controller/HomeController.java
+++ src/main/java/corona/controller/HomeController.java
@@ -1,17 +1,21 @@
 package corona.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @RequestMapping(method = RequestMethod.GET)
+
+    @RequestMapping( value = "/",method = RequestMethod.GET)
     public String homepage(Model model) {
         model.addAttribute("message", "welcome to corona fighter's club");
         model.addAttribute("message1", "welcome to homepage");
+
         return "homepage";
     }
+
 }
--- src/main/webapp/WEB-INF/pages/homepage.jsp
+++ src/main/webapp/WEB-INF/pages/homepage.jsp
@@ -1,2 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+
+<jsp:include page="homePage_Header.jsp"/>
+<br>
+<br>
+<br>
 <h1>${message}</h1>
 <h1>${message1}</h1>
+
