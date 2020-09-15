2b3b2ab7cbf60c2ed57c62258cb9551891d3f459
bala <balakrishnanshss@gmail.com>
2020-09-15 13:08:19 +0530

    commiting homepage

--- src/main/java/corona/controller/HomeController.java
+++ src/main/java/corona/controller/HomeController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
     @RequestMapping(method = RequestMethod.GET)
     public String homepage(Model model) {
         model.addAttribute("message", "welcome to corona fighter's club");
         model.addAttribute("message1", "welcome to homepage");
+        return "homepage";
+    }
+    @RequestMapping(value = "/homepage",method = RequestMethod.GET)
+    public String homePage()
+    {
         return "homepage";
     }
 }
